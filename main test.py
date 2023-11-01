e3b518706ef9a5aa51d37cb9e2a2b54f46c5bae6
ABZANMASTER <ASESINOELDIOS@users.noreply.github.com>
2023-11-01 12:12:27 +0200

    Add files via upload

---
+++ main test.py
@@ -0,0 +1,19 @@
+from matrixtest import *
+from inverse_algorithm import *
+import timeit
+from numba import set_num_threads
+#################
+#m,n are for the dimentions of A matrix
+# k is for the number of CPU threads to use
+# if k=1 it executes the algorithm serially
+def testfunc(n,m,k):
+    #creation of A matrix 
+    A=matrixtest(n,m)
+    #setting the number of threads 
+    set_num_threads(k)
+    #run the program to calculate the inverse algorithm 
+    inv_A=inv_unit_diagon_blocks(A,m)
+    return inv_A 
+
+if __name__ == '__main__':
+    testfunc()
---
+++ matrixtest.py
@@ -0,0 +1,13 @@
+import numpy as np
+def matrixtest(n,m):
+    typenum=np.complex64
+    S=np.empty((n,n),dtype=typenum)
+    S=np.random.random((n,n))
+    A=np.empty((m*m,n,n),dtype=typenum)
+    inv_s=inv(S)
+    inv_s=inv_s.astype(typenum)
+    for i in range (m*m):
+        x=np.random.random(n)
+        A[i]=S@np.diag(x)@inv_s
+    A=np.concatenate(A.reshape(m,m,n,n).swapaxes(1, 2).reshape(-1, n*m, n*m))
+    return A
